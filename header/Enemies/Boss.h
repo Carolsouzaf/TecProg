7fa6fc9277bfc0d9a173b9f807b07c5f8776da00
Carol <carolsouzaf@hotmail.com>
2021-10-13 11:15:31 -0300

    Basic structure for the game

---
+++ header/Enemies/Boss.h
@@ -0,0 +1,10 @@
+#ifndef BOSS_H
+#define BOSS_H
+class Boss{
+
+public:
+    Boss();
+    ~Boss();
+
+};
+#endif
---
+++ header/Stages/Stage2.h
@@ -0,0 +1,9 @@
+#ifndef STAGE2_H
+#define STAGE2_H
+class Stage2{
+
+public:
+    Stage2();
+    ~Stage2();
+};
+#endif
